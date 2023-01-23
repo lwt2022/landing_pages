32bb602d675e38a38495b1e4de62820b35717b1f
antonio jurado <iberico.cortador@gmail.com>
2023-01-23 22:49:14 +0900

    app.rb updated

--- project/app.rb
+++ project/app.rb
@@ -1,9 +1,17 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require 'tilt/erubis'
+require 'erubis'
 
 set :public_folder, File.expand_path("public", __dir__)
+set :views, File.expand_path("../views", __FILE__)
+set :layout_template, :layout
+set :erb, :engine_class => Erubis::FastEruby
+
+
+
 
 get '/' do
     erb :index, :layout => :layout
 end
+
+
