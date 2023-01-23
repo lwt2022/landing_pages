85c45d14b788d98f777d3d7d035e7ce55566d971
antonio jurado <iberico.cortador@gmail.com>
2023-01-23 23:22:03 +0900

    views folder moved to project folder

--- project/app.rb
+++ project/app.rb
@@ -3,15 +3,9 @@ require 'sinatra/reloader'
 require 'erubis'
 
 set :public_folder, File.expand_path("public", __dir__)
-set :views, File.expand_path("../views", __FILE__)
 set :layout_template, :layout
-set :erb, :engine_class => Erubis::FastEruby
-
-
-
+set :views, './views'
 
 get '/' do
     erb :index, :layout => :layout
 end
-
-
